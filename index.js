b8e931e3a8e4603b3b5f9d026ca2ae9d4346c79a
AsmaraHassan <hassanasmara939@gmail.com>
2024-07-06 17:33:47 +0500

    add

---
+++ index.js
@@ -0,0 +1,10 @@
+// / program to generate a multiplication table
+
+var userinput = +prompt('Please enter an integer: ');
+
+
+for(var i = 1; i <= 10; i++) {
+
+  
+    console.log(`${i} * ${i} = ${userinput}`);
+}
